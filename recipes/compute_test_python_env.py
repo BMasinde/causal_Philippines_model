3b3c17e948599d771bc77f1be9f21ec57b8aeb48
admin <masindeb@live.com>
2025-02-21 10:32:34 +0100

    Saved recipe: CAUSALPHILIPPINESDAMAGEPREDICTIONS.compute_test_python_env

---
+++ recipes/compute_test_python_env.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+mun_model_data = dataiku.Dataset("mun_model_data")
+mun_model_data_df = mun_model_data.get_dataframe()
+
+
+# Compute recipe outputs from inputs
+# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
+# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+
+test_python_env_df = mun_model_data_df # For this sample code, simply copy input to output
+
+
+# Write recipe outputs
+test_python_env = dataiku.Dataset("test_python_env")
+test_python_env.write_with_schema(test_python_env_df)
