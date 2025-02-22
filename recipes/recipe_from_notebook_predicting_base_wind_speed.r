9c31f5762d284f43873eabaed907448e5c703958
admin <masindeb@live.com>
2025-02-22 03:45:27 +0100

    Recipe CAUSALPHILIPPINESDAMAGEPREDICTIONS.recipe_from_notebook_predicting_base_wind_speed saved through notebook predicting_base_wind_speed

--- recipes/recipe_from_notebook_predicting_base_wind_speed.r
+++ recipes/recipe_from_notebook_predicting_base_wind_speed.r
@@ -1,7 +1,6 @@
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 library(dataiku)
 library(rpart)
-#library(mlflow)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Read the dataset as a R dataframe in memory
@@ -17,29 +16,93 @@ base_wind_max_model <- rpart(wind_max ~ track_min_dist,
                        method = "anova")
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# Save the trained model in a Managed Folder
-dkuManagedFolderPath <- dkuManagedFolderPath("scm_models")
-saveRDS(base_wind_max_model, file = paste0(dkuManagedFolderPath, "/base_wind_max_model.rds"))
+# Training structural equation for rain speed
+# rain_total = f(track_min_dist, eps)
+
+base_rain_total_model <- rpart(rain_total ~ track_min_dist,
+                       data = df_base_train,
+                       method = "anova")
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# Save the trained model in a Managed Folder
+## Adding the predicted parents' to the training dataset
+df_base_train <- df_base_train %>%
+  mutate(wind_max_pred = wind_max_pred, 
+         rain_total_pred = rain_total_pred
+         )
 
-# Start MLflow run
-#mlflow_start_run()
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Training decision tree for classification 
+damage_fit_class <- rpart(Y ~ wind_max_pred + 
+                           rain_total_pred + 
+                           roof_strong_wall_strong + 
+                           roof_strong_wall_light +
+                           roof_strong_wall_salv + 
+                           roof_light_wall_strong + 
+                           roof_light_wall_light + 
+                           roof_light_wall_salv + 
+                           roof_salv_wall_strong +
+                           roof_salv_wall_light +
+                           roof_salv_wall_salv +
+                           ls_risk_pct +
+                           ss_risk_pct +
+                           ruggedness_mean +
+                           slope_mean +
+                           wind_blue_ss +
+                           wind_yellow_ss +          
+                           wind_orange_ss +          
+                           wind_red_ss +
+                           rain_blue_ss +
+                           rain_yellow_ss +
+                           rain_orange_ss +
+                           rain_red_ss,
+                         method = "class", 
+                         data = df_base_train
+                         )
 
-# Save model locally (as a temporary directory or file)
-#model_path <- tempfile("mlflow_model_")
-#mlflow_save_model(model, model_path)
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Training decision tree for regression
+base_damage_fit_reg <- rpart(damage_perc ~ wind_max_pred + 
+                           rain_total_pred + 
+                           roof_strong_wall_strong + 
+                           roof_strong_wall_light +
+                           roof_strong_wall_salv + 
+                           roof_light_wall_strong + 
+                           roof_light_wall_light + 
+                           roof_light_wall_salv + 
+                           roof_salv_wall_strong +
+                           roof_salv_wall_light +
+                           roof_salv_wall_salv +
+                           ls_risk_pct +
+                           ss_risk_pct +
+                           ruggedness_mean +
+                           slope_mean +
+                           wind_blue_ss +
+                           wind_yellow_ss +          
+                           wind_orange_ss +          
+                           wind_red_ss +
+                           rain_blue_ss +
+                           rain_yellow_ss +
+                           rain_orange_ss +
+                           rain_red_ss,
+                         method = "class", 
+                         data = df_base_train
+                         )
 
-# Access the managed folder in Dataiku DSS
-#managed_folder <- dataiku::managedFolder("scm_models")
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+# Save the trained models in a Managed Folder
+dkuManagedFolderPath <- dkuManagedFolderPath("scm_base_models")
 
-# Define the path inside the managed folder
-#managed_folder_path <- file.path(managed_folder$getPath(), "base_wind_max_model")
+# Saving wind model
+saveRDS(base_wind_max_model, file = paste0(dkuManagedFolderPath, "/base_wind_max_model.rds"))
 
-# Copy the saved model to the managed folder
-#file.copy(model_path, managed_folder_path, recursive = TRUE)
+# Saving rain model
+saveRDS(base_rain_total_model, file = paste0(dkuManagedFolderPath, "/base_rain_total_model.rds"))
 
-# Log the parameters and metrics (optional)
-#mlflow_log_param("model_type", "Regression Tree")
-#mlflow_log_metric("RMSE", 0.95)
+# Saving classification model
+saveRDS(damage_fit_class, file = paste0(dkuManagedFolderPath, "/damage_fit_class_model.rds"))
+
+# Saving base regression model
+saveRDS(base_damage_fit_reg, file = paste0(dkuManagedFolderPath, "/base_damage_fit_reg_model.rds"))
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+colnames(df_base_train)
