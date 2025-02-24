235f3ae8a59887412228bb80863db78bba17b751
admin <masindeb@live.com>
2025-02-24 05:14:35 +0100

    Recipe CAUSALPHILIPPINESDAMAGEPREDICTIONS.recipe_from_notebook_validating_base_scm_models saved through notebook notebook editor for recipe_from_notebook_validating_base_scm_models

--- recipes/recipe_from_notebook_validating_base_scm_models.r
+++ recipes/recipe_from_notebook_validating_base_scm_models.r
@@ -4,10 +4,22 @@ library(rpart)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Recipe inputs
-scm_base_models <- dkuManagedFolderPath("XxDUuuYe")
+scm_base_models <- dkuManagedFolderPath("scm_base_models")
 base_validation <- dkuReadDataset("base_validation", samplingMethod="head", nbRows=100000)
 
+# Reading .rds files
+# Construct the full file path
+wind_path <- file.path(scm_base_models, "base_wind_max_model.rds")
+
+rain_path  <- file.path(scm_base_models, "base_rain_total_model.rds")
+
+damage_path  <- file.path(scm_base_models, "damage_rain_total_model.rds")
 
 
+
+# Read the RDS file
+model <- readRDS(model_path)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Recipe outputs
 base_val_scm_models <- dkuManagedFolderPath("ZMqSlX9h")
