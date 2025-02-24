979c05d7d3d4a6b2376bab5c336f74476d02b365
admin <masindeb@live.com>
2025-02-24 14:35:50 +0100

    Saved recipe: CAUSALPHILIPPINESDAMAGEPREDICTIONS.compute_5NPBmWH1

---
+++ recipes/compute_5NPBmWH1.r
@@ -0,0 +1,11 @@
+library(dataiku)
+
+# Recipe inputs
+base_scm_clas_min_model <- dkuManagedFolderPath("xcPrnvPS")
+base_scm_reg_min_model <- dkuManagedFolderPath("ZijSaAqQ")
+trunk_scm_min_model <- dkuManagedFolderPath("dL4i4SKb")
+
+
+
+# Recipe outputs
+hurdle_predictions_testing <- dkuManagedFolderPath("5NPBmWH1")
