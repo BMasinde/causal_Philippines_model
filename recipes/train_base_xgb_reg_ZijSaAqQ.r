9bd35ecbe9d4ea6a56cef01518227f41a3de7d55
admin <masindeb@live.com>
2025-03-11 10:41:32 +0100

    Recipe CAUSAL_PHILIPPINES_DAMAGE_PREDICTIONS_UNADJUSTED_FULL_SCM.train_base_xgb_reg_ZijSaAqQ saved through notebook base reg training (ZijSaAqQ)

--- recipes/train_base_xgb_reg_ZijSaAqQ.r
+++ recipes/train_base_xgb_reg_ZijSaAqQ.r
@@ -31,15 +31,15 @@ cat("number of rows in combined train data:", nrow(df_base_train2), sep = " ")
 # we will need to also include island_groups
 # in the final outcome prediction model to adjust for the confounding
 
-base_track_model  <- rpart(track_min_dist  ~ island_groups,
-                          data = df_base_train2,
-                          method = "anova")
+#base_track_model  <- rpart(track_min_dist  ~ island_groups,
+#                          data = df_base_train2,
+#                          method = "anova")
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Training structural equation for wind speed
 # wind_speed = f(track_min_dist, eps)
 
-base_wind_model <- rpart(wind_max ~ track_min_dist + island_groups,
+base_wind_model <- rpart(wind_max ~ track_min_dist,
                        data = df_base_train2,
                        method = "anova")
 
@@ -47,44 +47,47 @@ base_wind_model <- rpart(wind_max ~ track_min_dist + island_groups,
 # Training structural equation for rain speed
 # rain_total = f(track_min_dist, eps)
 
-base_rain_model <- rpart(rain_total ~ track_min_dist + island_groups,
+base_rain_model <- rpart(rain_total ~ track_min_dist,
                        data = df_base_train2,
                        method = "anova")
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Building typologies are determined by region
-base_roof_strong_wall_strong_model  <- rpart(roof_strong_wall_strong  ~ island_groups,
-                                             data = df_base_train2,
-                                            method = "anova")
+#base_roof_strong_wall_strong_model  <- rpart(roof_strong_wall_strong  ~ island_groups,
+#                                             data = df_base_train2,
+#                                            method = "anova")
 
-base_roof_strong_wall_light_model  <- rpart(roof_strong_wall_light ~ island_groups,
-                                           data = df_base_train2,
-                                           method = "anova")
+#base_roof_strong_wall_light_model  <- rpart(roof_strong_wall_light ~ island_groups,
+#                                           data = df_base_train2,
+#                                           method = "anova")
 
-base_roof_strong_wall_salv_model  <- rpart(roof_strong_wall_salv ~ island_groups,
-                                          data = df_base_train2,
-                                          method = "anova")
-base_roof_light_wall_strong_model  <- rpart(roof_light_wall_strong ~ island_groups,
-                                           data = df_base_train2,
-                                           method = "anova")
-base_roof_light_wall_light_model  <- rpart(roof_light_wall_light ~ island_groups,
-                                          data = df_base_train2,
-                                          method = "anova")
-base_roof_light_wall_salv_model  <- rpart(roof_light_wall_salv ~ island_groups,
-                                         data = df_base_train2,
-                                         method = "anova")
+#base_roof_strong_wall_salv_model  <- rpart(roof_strong_wall_salv ~ island_groups,
+#                                          data = df_base_train2,
+#                                          method = "anova")
 
-base_roof_salv_wall_strong_model  <- rpart(roof_salv_wall_strong ~ island_groups,
-                                          data = df_base_train2,
-                                          method = "anova")
+#base_roof_light_wall_strong_model  <- rpart(roof_light_wall_strong ~ island_groups,
+#                                           data = df_base_train2,
+#                                           method = "anova")
 
-base_roof_salv_wall_light_model  <- rpart(roof_salv_wall_light ~ island_groups,
-                                  data = df_base_train2,
-                                  method = "anova")
+#base_roof_light_wall_light_model  <- rpart(roof_light_wall_light ~ island_groups,
+#                                          data = df_base_train2,
+#                                          method = "anova")
 
-base_roof_salv_wall_salv_model  <- rpart(roof_salv_wall_salv ~ island_groups,
-                                  data = df_base_train2,
-                                  method = "anova")
+#base_roof_light_wall_salv_model  <- rpart(roof_light_wall_salv ~ island_groups,
+#                                         data = df_base_train2,
+#                                         method = "anova")
+
+#base_roof_salv_wall_strong_model  <- rpart(roof_salv_wall_strong ~ island_groups,
+#                                          data = df_base_train2,
+#                                          method = "anova")
+
+#base_roof_salv_wall_light_model  <- rpart(roof_salv_wall_light ~ island_groups,
+#                                  data = df_base_train2,
+#                                  method = "anova")
+
+#base_roof_salv_wall_salv_model  <- rpart(roof_salv_wall_salv ~ island_groups,
+#                                  data = df_base_train2,
+#                                  method = "anova")
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # # Adding the predicted parents' to the training dataset
@@ -98,18 +101,18 @@ base_roof_salv_wall_salv_model  <- rpart(roof_salv_wall_salv ~ island_groups,
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 model_list <- list(
-  track_min_dist = base_track_model,
+  #track_min_dist = base_track_model,
   wind_max = base_wind_model,
   rain_total = base_rain_model,
-  roof_strong_wall_strong = base_roof_strong_wall_strong_model,
-  roof_strong_wall_light = base_roof_strong_wall_light_model,
-  roof_strong_wall_salv = base_roof_strong_wall_salv_model,
-  roof_light_wall_strong = base_roof_light_wall_strong_model,
-  roof_light_wall_light = base_roof_light_wall_light_model,
-  roof_light_wall_salv = base_roof_light_wall_salv_model,
-  roof_salv_wall_strong = base_roof_salv_wall_strong_model,
-  roof_salv_wall_light = base_roof_salv_wall_light_model,
-  roof_salv_wall_salv = base_roof_salv_wall_salv_model
+  #roof_strong_wall_strong = base_roof_strong_wall_strong_model,
+  #roof_strong_wall_light = base_roof_strong_wall_light_model,
+  #roof_strong_wall_salv = base_roof_strong_wall_salv_model,
+  #roof_light_wall_strong = base_roof_light_wall_strong_model,
+  #roof_light_wall_light = base_roof_light_wall_light_model,
+  #roof_light_wall_salv = base_roof_light_wall_salv_model,
+  #roof_salv_wall_strong = base_roof_salv_wall_strong_model,
+  #roof_salv_wall_light = base_roof_salv_wall_light_model,
+  #roof_salv_wall_salv = base_roof_salv_wall_salv_model
 )
 
 # Apply predictions efficiently
@@ -124,84 +127,6 @@ rain_fractions <- c("blue_ls_frac", "yellow_ls_frac", "orange_ls_frac", "red_ls_
 df_base_train2 <- df_base_train2 %>%
   mutate(across(all_of(wind_fractions), ~ . * wind_max_pred, .names = "wind_{.col}"),
          across(all_of(rain_fractions), ~ . * rain_total_pred, .names = "rain_{.col}"))
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# # parameter tuning
-# # Define a grid of hyperparameters
-# cp_values <- seq(0.0001, 0.05, by = 0.0005)
-# maxdepth_values <- c(3, 5, 7, 10)
-# minsplit_values <- c(10, 20, 30, 40)
-# minbucket_values <- c(5, 10, 20)
-
-# # Create an empty list to store results
-# # Create an empty list to store results
-# results <- data.frame(cp = numeric(), maxdepth = numeric(),
-#                       minsplit = numeric(), minbucket = numeric(), RMSE = numeric())
-
-# # predicting for wind and rainfall for the validation dataset
-# df_val_base_tune <- base_validation %>%
-#   mutate(
-#     wind_max_pred = predict(
-#       base_wind_model, newdata = base_validation),
-#     rain_total_pred = predict(
-#       base_rain_model,
-#       newdata = base_validation)
-#     )
-
-# # Train the model using manual grid search
-# grid_id <- 1  # Index for list storage
-
-# # Iterate over all combinations of hyperparameters
-# for (cp in cp_values) {
-#   for (maxdepth in maxdepth_values) {
-#     for (minsplit in minsplit_values) {
-#       for (minbucket in minbucket_values) {
-
-#         # Train the model with specific hyperparameters
-#         model <- rpart(
-#           damage_perc ~ wind_max_pred +
-#             rain_total_pred +
-#             roof_strong_wall_strong +
-#             roof_strong_wall_light +
-#             roof_strong_wall_salv +
-#             roof_light_wall_strong +
-#             roof_light_wall_light +
-#             roof_light_wall_salv +
-#             roof_salv_wall_strong +
-#             roof_salv_wall_light +
-#             roof_salv_wall_salv +
-#             ls_risk_pct +
-#             ss_risk_pct +
-#             wind_blue_ss +
-#             wind_yellow_ss +
-#             wind_orange_ss +
-#             wind_red_ss +
-#             rain_blue_ss +
-#             rain_yellow_ss +
-#             rain_orange_ss +
-#             rain_red_ss,
-#           data = df_base_train,
-#           method = "anova",  # Regression
-#           control = rpart.control(cp = cp, maxdepth = maxdepth,
-#                                   minsplit = minsplit, minbucket = minbucket)
-#         )
-
-#         # Make predictions on the validation set
-#         val_predictions <- predict(model, newdata = df_val_base_tune)
-
-#         # Compute RMSE
-#         rmse_value <- rmse(df_val_base_tune$damage_perc, val_predictions)
-
-#         # Store results
-#         results <- rbind(results, data.frame(cp, maxdepth, minsplit, minbucket, RMSE = rmse_value))
-#       }
-#     }
-#   }
-# }
-
-# # Print the best hyperparameter combination
-# best_params <- results[which.min(results$RMSE), ]
-# print(best_params)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # setting seed for reproducibility
@@ -235,17 +160,17 @@ registerDoMC(cores = num_cores)  # Enable parallel processing
 system.time({
 # Train the model using grid search with 7-fold CV
 base_xgb_reg_model <- train(
-  damage_perc ~ track_min_dist_pred + # Confounder adjustmentwind_max_pred +
+  damage_perc ~ track_min_dist + # Confounder adjustmentwind_max_pred +
     rain_total_pred +
-    roof_strong_wall_strong_pred +
-    roof_strong_wall_light_pred +
-    roof_strong_wall_salv_pred +
-    roof_light_wall_strong_pred +
-    roof_light_wall_light_pred +
-    roof_light_wall_salv_pred +
-    roof_salv_wall_strong_pred +
-    roof_salv_wall_light_pred +
-    roof_salv_wall_salv_pred +
+    roof_strong_wall_strong +
+    roof_strong_wall_light +
+    roof_strong_wall_salv +
+    roof_light_wall_strong +
+    roof_light_wall_light +
+    roof_light_wall_salv +
+    roof_salv_wall_strong +
+    roof_salv_wall_light +
+    roof_salv_wall_salv +
     #ls_risk_pct + # Not local variables
     #ss_risk_pct +
     wind_blue_ss +
@@ -255,8 +180,7 @@ base_xgb_reg_model <- train(
     rain_blue_ss +
     rain_yellow_ss +
     rain_orange_ss +
-    rain_red_ss +
-    island_groups,  # Confounder adjustment
+    rain_red_ss,
   data = df_base_train2,
   method = "xgbTree",
   trControl = train_control,
@@ -269,44 +193,6 @@ Sys.sleep(2)  # This is just an example to simulate a delay
 print(base_xgb_reg_model$bestTune)
 
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
-# # Training based on tuned parameters
-
-# # Combine Training and Validation datasets for final training
-
-# final_training_df  <- rbind(df_base_train,
-#                            df_val_base_tune)
-
-
-# damage_fit_reg_min <- rpart(damage_perc ~ wind_max_pred +
-#                               rain_total_pred +
-#                               roof_strong_wall_strong +
-#                               roof_strong_wall_light +
-#                               roof_strong_wall_salv +
-#                               roof_light_wall_strong +
-#                               roof_light_wall_light +
-#                               roof_light_wall_salv +
-#                               roof_salv_wall_strong +
-#                               roof_salv_wall_light +
-#                               roof_salv_wall_salv +
-#                               ls_risk_pct +
-#                               ss_risk_pct +
-#                               wind_blue_ss +
-#                               wind_yellow_ss +
-#                               wind_orange_ss +
-#                               wind_red_ss +
-#                               rain_blue_ss +
-#                               rain_yellow_ss +
-#                               rain_orange_ss +
-#                               rain_red_ss,
-#                               method = "anova",
-#                               control = rpart.control(cp = best_params$cp,
-#                                                       maxdepth = best_params$maxdepth,
-#                                                       minsplit = best_params$minsplit,
-#                                                       minbucket = best_params$minbucket),
-#                               data = final_training_df
-#                          )
-
-# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # ----------------------       Logging model performanc using MLFLOW ------------------
 
 # set tracking URI
@@ -316,7 +202,7 @@ mlflow_set_tracking_uri("http://127.0.0.1:5000")
 suppressWarnings(try(mlflow_end_run(), silent = TRUE))
 
 # Logging metrics for model training and the parameters used
-mlflow_set_experiment(experiment_name = "SCM - XGBOOST base regression - CV (Training metircs)")
+mlflow_set_experiment(experiment_name = "U-SCM - XGBOOST base regression - CV (Training metircs)")
 
 # Ensure that MLflow has only one run. Start MLflow run once.
 run_name <- paste("XGBoost Run", Sys.time())  # Unique name using current time
@@ -340,15 +226,15 @@ for (param in names(best_params)) {
 # ---------- train using best parameters
 damage_fit_reg_min <- train(damage_perc ~ wind_max_pred +
                               rain_total_pred +
-                              roof_strong_wall_strong_pred +
-                              roof_strong_wall_light_pred +
-                              roof_strong_wall_salv_pred +
-                              roof_light_wall_strong_pred +
-                              roof_light_wall_light_pred +
-                              roof_light_wall_salv_pred +
-                              roof_salv_wall_strong_pred +
-                              roof_salv_wall_light_pred +
-                              roof_salv_wall_salv_pred +
+                              roof_strong_wall_strong +
+                              roof_strong_wall_light +
+                              roof_strong_wall_salv +
+                              roof_light_wall_strong +
+                              roof_light_wall_light +
+                              roof_light_wall_salv +
+                              roof_salv_wall_strong +
+                              roof_salv_wall_light +
+                              roof_salv_wall_salv +
                               #ls_risk_pct +
                               #ss_risk_pct +
                               wind_blue_ss +
@@ -359,8 +245,7 @@ damage_fit_reg_min <- train(damage_perc ~ wind_max_pred +
                               rain_yellow_ss +
                               rain_orange_ss +
                               rain_red_ss +
-                              island_groups +  # Confounder adjustment
-                              track_min_dist_pred, # Confounder adjustment
+                              track_min_dist, # Confounder adjustment
                               method = "xgbTree",
                               trControl = trainControl(method = "none"),
                               tuneGrid = best_params, # Use the best parameters here
@@ -431,30 +316,30 @@ managed_folder_path <- dkuManagedFolderPath("ZijSaAqQ")
 models <- list(damage_fit_reg_min,
                base_wind_model,
                base_rain_model,
-               base_track_model,
-               base_roof_strong_wall_strong_model,
-               base_roof_strong_wall_light_model,
-               base_roof_strong_wall_salv_model,
-               base_roof_light_wall_strong_model,
-               base_roof_light_wall_light_model,
-               base_roof_light_wall_salv_model,
-               base_roof_salv_wall_strong_model,
-               base_roof_salv_wall_light_model,
-               base_roof_salv_wall_salv_model
+               #base_track_model,
+               #base_roof_strong_wall_strong_model,
+               #base_roof_strong_wall_light_model,
+               #base_roof_strong_wall_salv_model,
+               #base_roof_light_wall_strong_model,
+               #base_roof_light_wall_light_model,
+               #base_roof_light_wall_salv_model,
+               #base_roof_salv_wall_strong_model,
+               #base_roof_salv_wall_light_model,
+               #base_roof_salv_wall_salv_model
               )
 model_names <- c("base_reg_model",
                  "base_wind_model",
                  "base_rain_model",
-                 "base_track_model",
-                 "base_roof_strong_wall_strong_model",
-                 "base_roof_strong_wall_light_model",
-                 "base_roof_strong_wall_salv_model",
-                 "base_roof_light_wall_strong_model",
-                 "base_roof_light_wall_light_model",
-                 "base_roof_light_wall_salv_model",
-                 "base_roof_salv_wall_strong_model",
-                 "base_roof_salv_wall_light_model",
-                 "base_roof_salv_wall_salv_model"
+                 #"base_track_model",
+                 #"base_roof_strong_wall_strong_model",
+                 #"base_roof_strong_wall_light_model",
+                 #"base_roof_strong_wall_salv_model",
+                 #"base_roof_light_wall_strong_model",
+                 #"base_roof_light_wall_light_model",
+                 #"base_roof_light_wall_salv_model",
+                 #"base_roof_salv_wall_strong_model",
+                 #"base_roof_salv_wall_light_model",
+                 #"base_roof_salv_wall_salv_model"
                 )
 
 #----------------------- Saving trained XGBOOST model ----------------------------------------
