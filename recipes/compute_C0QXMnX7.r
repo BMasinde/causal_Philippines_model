5f3dfd357ce71e70f907f4fb81e5ec99abf5d271
admin <masindeb@live.com>
2025-03-31 10:45:26 +0200

    Saved recipe: CAUSAL_PHILIPPINES_DAMAGE_PREDICTIONS_FULL_SCM.compute_C0QXMnX7

---
+++ recipes/compute_C0QXMnX7.r
@@ -0,0 +1,10 @@
+library(dataiku)
+
+# Recipe inputs
+counterfactual_test_data <- dkuReadDataset("counterfactual_test_data", samplingMethod="head", nbRows=100000)
+hurdle_predictions_testing <- dkuManagedFolderPath("5NPBmWH1")
+
+
+
+# Recipe outputs
+matching_counterfactuals <- dkuManagedFolderPath("C0QXMnX7")
