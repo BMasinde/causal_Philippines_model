f5d2192d3ae5f7a29b26d4e61d888817b80b15b8
admin <masindeb@live.com>
2025-05-02 22:06:55 +0200

    Saved recipe: CAUSAL_PHILIPPINES_DAMAGE_PREDICTIONS_UNADJUSTED_FULL_SCM.compute_K9arRZ6K

---
+++ recipes/compute_K9arRZ6K.r
@@ -0,0 +1,11 @@
+library(dataiku)
+
+# Recipe inputs
+ass_XGBOOST_base_reg <- dkuManagedFolderPath("DFdq9elg")
+ass_XGBOOST_classifier <- dkuManagedFolderPath("fZ8zhmA4")
+ass_XGBOOST_trunc_reg <- dkuManagedFolderPath("ZGOs5kwX")
+
+
+
+# Recipe outputs
+ass_hurdle_predictions <- dkuManagedFolderPath("K9arRZ6K")
